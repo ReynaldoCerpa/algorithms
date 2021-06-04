550db0b8f056adc774c20158d7c0f1bd111afa2f
ReynaldoCerpa <reynaldocerpa07@gmail.com>
2021-06-04 10:54:29 -0700

    simple server setup for algorithms in js

---
+++ public/algorithms.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+/*
+// Reverse string
+const result = reverseString("hello");
+console.log(result);
+
+function reverseString(text){
+    let reverse = "";
+    let x;
+    let length = text.length;
+    
+    for(x in text){
+        reverse += text.charAt((length-x)-1);
+    }
+    return reverse;
+} */
